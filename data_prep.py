7b2c81c477c5ffacbd26f0ddc2613dcc8aa80f1d
jonaslee17 <uhz7mq@virginia.edu>
2026-02-15 12:52:48 -0500

    update collapse

--- data_prep.py
+++ data_prep.py
@@ -1,14 +1,11 @@
 import pandas as pd
 
-# Update this path to the folder where your CSVs are stored
 folder_path = "/Users/jonaslee/Desktop/cvrj-bed-forecasting/cvrj_dataset_2012-25.csv"
 output_file = "cvrj_dataset_v2.csv"
 
-
-df_list = []    
 df = pd.read_csv(folder_path)
 
-# Some files have offense_code vs offense code
+# some files have offense_code vs offense code
 df.columns = [c.strip().replace('_', ' ').replace('\n', ' ') for c in df.columns]
     
 if 'Offense Code' not in df.columns:
@@ -17,7 +14,6 @@ if 'Offense Code' not in df.columns:
             df.rename(columns={col: 'Offense Code'}, inplace=True)
             break
     
-df_list.append(df)
 # extracts severity char
 df['severity_char'] = df['Offense Code'].astype(str).str[-2].str.upper()
 
